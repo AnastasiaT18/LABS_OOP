59b08f3a2b8d9c4253bafc1f037eb020f3b8dbfb
Anastasia <anastasia.tiganescu@isa.utm.md>
2024-12-01 10:18:49 +0200

    Made small changed to scheduled tasks. Now it automatically deletes processed files from queue.

--- labs/java-classifcation/src/main/java/lab3/task5/ScheduledTasks.java
+++ labs/java-classifcation/src/main/java/lab3/task5/ScheduledTasks.java
@@ -1,6 +1,7 @@
 package lab3.task5;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lab3.task1.ArrayImpl;
 import lab3.task1.ListImpl;
@@ -15,8 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Component
 public class ScheduledTasks {
@@ -24,30 +24,41 @@ public class ScheduledTasks {
     ObjectMapper mapper = new ObjectMapper();
     FolderReader folderReader = new FolderReader();
     Semaphore semaphore = initialize();
-    List<String> processed = new ArrayList<>();
     StatsTracker stats = new StatsTracker();
+
 
     @Scheduled(fixedRate = 5000)
     public void readCars() throws IOException {
+        long startTime = System.nanoTime();
         File[] list = folderReader.readDir("src/main/java/lab-car-service/queue");
         for(File file: list){
-            if(processed.contains(file.getName())){
-                continue;
-            }
             Car car = mapper.readValue(file,Car.class);
             stats.updateStats(car);
             semaphore.guideCar(car);
-            processed.add(file.getName());
+            file.delete();
         }
+
+        long endTime = System.nanoTime();
+        double elapsedTimeMs = (endTime - startTime) / 1_000_000.0;
+
+        System.out.println("readCars task executed in: " + elapsedTimeMs + " ms");
+
     }
+
 
     @Scheduled(fixedRate = 7500)
     public void serveCars(){
+        long startTime = System.nanoTime();
         System.out.println("-------Serving-------");
         semaphore.getGasRobots().serveCars();
         semaphore.getElecPeople().serveCars();
         semaphore.getElecRobots().serveCars();
         semaphore.getGasPeople().serveCars();
+
+        long endTime = System.nanoTime(); // End timing
+        double elapsedTimeMs = (endTime - startTime) / 1_000_000.0;
+
+        System.out.println("serveCars task executed in: " + elapsedTimeMs + " ms");
 
     }
 
@@ -81,13 +92,13 @@ public class ScheduledTasks {
     @PreDestroy
     public void printStats(){
         stats.printStats();
-        System.out.println("ELECTRIC: " +semaphore.getElecPeople().getRefuelingService().getCountCars());
-        System.out.println("GAS: " +semaphore.getGasRobots().getRefuelingService().getCountCars());
-        System.out.println("PEOPLE: " +semaphore.getElecPeople().getDiningService().getCountCars());
-        System.out.println("ROBOTS: " +semaphore.getGasRobots().getDiningService().getCountCars());
-        System.out.println("DINING: " +(semaphore.getGasRobots().getDiningService().getCountDining() + semaphore.getElecPeople().getDiningService().getCountDining()));
-        System.out.println("NOT DINING: " + (semaphore.getElecPeople().getRefuelingService().getCountCars() + semaphore.getGasRobots().getRefuelingService().getCountCars() -semaphore.getGasRobots().getDiningService().getCountDining() - semaphore.getElecPeople().getDiningService().getCountDining()));
-        System.out.println("GAS(Consumption): " +semaphore.getGasRobots().getRefuelingService().getConsumption());
-        System.out.println("ELECTRIC(Consumption): " +semaphore.getElecRobots().getRefuelingService().getConsumption());
+//        System.out.println("ELECTRIC: " +semaphore.getElecPeople().getRefuelingService().getCountCars());
+//        System.out.println("GAS: " +semaphore.getGasRobots().getRefuelingService().getCountCars());
+//        System.out.println("PEOPLE: " +semaphore.getElecPeople().getDiningService().getCountCars());
+//        System.out.println("ROBOTS: " +semaphore.getGasRobots().getDiningService().getCountCars());
+//        System.out.println("DINING: " +(semaphore.getGasRobots().getDiningService().getCountDining() + semaphore.getElecPeople().getDiningService().getCountDining()));
+//        System.out.println("NOT DINING: " + (semaphore.getElecPeople().getRefuelingService().getCountCars() + semaphore.getGasRobots().getRefuelingService().getCountCars() -semaphore.getGasRobots().getDiningService().getCountDining() - semaphore.getElecPeople().getDiningService().getCountDining()));
+//        System.out.println("GAS(Consumption): " +semaphore.getGasRobots().getRefuelingService().getConsumption());
+//        System.out.println("ELECTRIC(Consumption): " +semaphore.getElecRobots().getRefuelingService().getConsumption());
     }
 }
